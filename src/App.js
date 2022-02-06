be60507546e52704988dcde953d6e71a0d6a9e71
doctorrWeb <doctorrweb@gmail.com>
2022-02-06 19:05:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import { 
+  useState,
+  useReducer,
+  useEffect,
+  // useLayoutEffect,
+  // useContext,
+  // useCallback,
+  // useMemo
+} from 'react'
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [ inputValue, setInputValue ] = useState('')
+  const [ name, setName ] = useState('')
+  const [ currency, setCurrency ] = useState('')
+  const [ currencyData, setCurrencyData ] = useState(null)
+
+  const [sum, dispatch] = useReducer((state, action) => { return state + action }, 0)
+
+  useEffect(() => {
+    axios
+      // .get('https://api.jikan.moe/v3/search/anime?q=naruto')
+      // .then(({data}) => console.log(data.results))
+
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(({data}) => data.bpi)
+      .then(data => setCurrencyData(data[currency]))
+
+    // fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+  }, [currency])
+
+  useEffect(() => {
+    console.log('currency', currency)
+  }, [currency])
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,15 +45,58 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        { name && <p>  My name is {name}. </p> }
+        {sum}
+        <form>
+
+          <input name='myInput' value={inputValue} onChange={e => setInputValue(e.target.value)} />
+
+          <button 
+            type='submit' 
+            onClick={(e) => {
+              e.preventDefault()
+              setName(inputValue)
+              setInputValue('')
+            }}
+          >
+            Submit
+          </button>
+        </form>
+
+        <button onClick={() => dispatch(1)}>
+        Add 1
+      </button>
+        
+      <select onChange={(e) => setCurrency(e.target.value)}>
+        <option></option>
+        <option>EUR</option>
+        <option>GBP</option>
+        <option>USD</option>
+      </select>
+
+      {
+        currencyData && (
+          <div>
+            <p>Currency: {currencyData.code} {currencyData.symbol}</p>
+            <p>Description: {currencyData.description}</p>
+            <p>Rate: {currencyData.rate}, Ratefloat: {currencyData.rate_float}</p>
+          </div>
+        )
+      }
+
+      {/* <button onClick={(e) => setCurrency(e.target.value)}>
+        Euro
+      </button>
+      <button onClick={(e) => setCurrency(e.target.value)}>
+        GBP
+      </button>
+      <button onClick={(e) => setCurrency(e.target.value)}>
+        USD
+      </button> */}
+
       </header>
+
+
     </div>
   );
 }
